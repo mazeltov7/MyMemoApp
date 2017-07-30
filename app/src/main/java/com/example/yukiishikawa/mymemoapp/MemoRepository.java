bb772465dfee2d5ddcad871e015185eb0af30167
Yuki Ishikawa <ishikawayuki2@gmail.com>
2017-07-31 00:03:52 +0900

    wip

--- app/src/main/java/com/example/yukiishikawa/mymemoapp/MemoRepository.java
+++ app/src/main/java/com/example/yukiishikawa/mymemoapp/MemoRepository.java
@@ -19,6 +19,71 @@ public class MemoRepository {
     // ファイル名フォーマット prefix-yyyy-mm-dd-HH-MM-SS.txt
     private static final String MEMO_FILE_FORMAT = "%1$s-%2$tF-%2$tH-%2$tM-%2$tS.txt";
 
+    // インスタンスを作らせない
+    private MemoRepository() {}
+
+    // メモの出力先ディレクトリを取得する
+    private static File getOutputDir(Context context) {
+        File outputDir;
+        if (Build.VERSION.SDK_INT >= 19) {
+            outputDir = context.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        } else {
+            outputDir = new File(context.getExternalFilesDir(null), "Documents");
+        }
+
+        if (outputDir == null) {
+            return null;
+        }
+
+        boolean isExist = true;
+
+        if (!outputDir.exists() || !outputDir.isDirectory()) {
+            isExist = outputDir.mkdirs();
+        }
+
+        if (isExist) {
+            return outputDir;
+        } else {
+            // ディレクトリの作成に失敗した場合
+            return null;
+        }
+    }
+
+    // 出力先ファイルを取得する
+    public static File getFileName(Context context, File outputDir) {
+        String fileNamePrefix = SettingPrefUtil.getKeyFileNamePrefix(context);
+
+        Calendar now = Calendar.getInstance();
+
+        String fileName = String.format(MEMO_FILE_FORMAT, fileNamePrefix, now);
+
+        return new File(outputDir, fileName);
+    }
+
+    // ファイルにメモを書き込む
+    private static boolean writeToFile(File outputFile, String memo) {
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(outputFile);
+            writer.write(memo);
+            writer.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return true;
+    }
+
     // ファイルとして保存し、データベースに保存する
     public static Uri store(Context context, String memo) {
         File outputDir;
